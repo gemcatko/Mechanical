c9f0e2c4c79a519384066fef5f4975d95a62ae2b
Solcansky <solcanma@at-work.local>
2020-03-14 18:17:54 +0100

    upper and reversed in main

---
+++ main.py
@@ -0,0 +1,12 @@
+def reverser_and_upper():
+    """
+
+    :return:
+    """
+
+
+
+if __name__ == "__main__":
+    string = "Smakyna"
+    print(string.upper())
+    print(''.join(reversed(string)))
