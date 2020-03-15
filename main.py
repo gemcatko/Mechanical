5b255d2dc4c55430397247bca845ff380ee73531
Solcansky <solcanma@at-work.local>
2020-03-15 19:08:32 +0100

    fixed lintner code style and docstrings warnings

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
+"""Some documentation."""
 import argparse
 
+
 def reverse_and_upper():
-    """
-    :return:
-    is returning uppercased  and reversed string
-    """
+    """Return uppercased and reversed string."""
     ap = argparse.ArgumentParser()
-    ap.add_argument("-s", "--string", required=True, help="Please enter, string you would like to uppercase and reverse ")
+    ap.add_argument("-s", "--string", required=True, help="Please enter, "
+                    "string you would like to uppercase andreverse ")
     args = vars(ap.parse_args())
     string = args["string"]
-    return (''.join(reversed(string.upper())))
+    return ''.join(reversed(string.upper()))
 
 
 if __name__ == "__main__":
